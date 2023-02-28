8439b144d82bb9fd71457d019f4366daf545805a
smonies <smonies@l-smonies.rhc.local>
2023-02-28 12:56:53 +0300

    test

---
+++ Object Repository/div_202120202019201820172016201520142013201_ea8a60.rs
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_202120202019201820172016201520142013201_ea8a60</name>
+   <tag></tag>
+   <elementGuidId>43636ee0-5177-4de8-951f-c71b984129a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#rowYear</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='rowYear']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>5dec58b1-62e9-4481-8648-2ba7e2b019aa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-lg-12 form-group</value>
+      <webElementGuid>482a8d3b-58ed-4956-9f2d-dd9c07bae019</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>rowYear</value>
+      <webElementGuid>152d8db0-7e17-4754-9644-14e489c49c52</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                السنوات
+                                
+                                    
+                                2021
+2020
+2019
+2018
+2017
+2016
+2015
+2014
+2013
+2012
+2011
+2010
+2009
+2008
+اختر السنة
+                                
+                            </value>
+      <webElementGuid>04d4d2db-4d47-4d21-b8de-7dd1e214e069</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rowYear&quot;)</value>
+      <webElementGuid>0e33205a-cec9-4401-b414-ea9e09655fb5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='rowYear']</value>
+      <webElementGuid>35d4354e-bdf2-401f-85ed-022fb9bfd57e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='معلومات شهادة الراتب'])[1]/following::div[5]</value>
+      <webElementGuid>f5d82f8f-954a-4707-b1db-8c6bbafba3ca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='طلب شهادة راتب'])[1]/following::div[8]</value>
+      <webElementGuid>791f8745-8a19-43cf-a3d7-e57312cc289f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div</value>
+      <webElementGuid>67456670-e10d-4b00-b939-8d971321ac38</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'rowYear' and (text() = '
+                                السنوات
+                                
+                                    
+                                2021
+2020
+2019
+2018
+2017
+2016
+2015
+2014
+2013
+2012
+2011
+2010
+2009
+2008
+اختر السنة
+                                
+                            ' or . = '
+                                السنوات
+                                
+                                    
+                                2021
+2020
+2019
+2018
+2017
+2016
+2015
+2014
+2013
+2012
+2011
+2010
+2009
+2008
+اختر السنة
+                                
+                            ')]</value>
+      <webElementGuid>bfdc4dd3-fc68-4f3b-9f6b-52c5f677deca</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
